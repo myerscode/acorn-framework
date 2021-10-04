25406d9d5733eed385e957b5d3a649c226e56749
Fred Myerscough <oniice@gmail.com>
2021-10-04 13:53:02 +0100

    feature: added display helpers to output interface

--- src/Foundation/Console/VoidOutput.php
+++ src/Foundation/Console/VoidOutput.php
@@ -8,4 +8,44 @@ use Symfony\Component\Console\Output\NullOutput;
 class VoidOutput extends NullOutput implements ConsoleOutputInterface
 {
 
+    public function text($message)
+    {
+        // do nothing
+    }
+
+    public function comment($message)
+    {
+        // do nothing
+    }
+
+    public function success($message)
+    {
+        // do nothing
+    }
+
+    public function error($message)
+    {
+        // do nothing
+    }
+
+    public function warning($message)
+    {
+        // do nothing
+    }
+
+    public function note($message)
+    {
+        // do nothing
+    }
+
+    public function info($message)
+    {
+        // do nothing
+    }
+
+    public function caution($message)
+    {
+        // do nothing
+    }
+
 }
--- src/Framework/Console/ConsoleOutputInterface.php
+++ src/Framework/Console/ConsoleOutputInterface.php
@@ -6,5 +6,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 interface ConsoleOutputInterface extends OutputInterface
 {
+    public function text($message);
 
+    public function comment($message);
+
+    public function success($message);
+
+    public function error($message);
+
+    public function warning($message);
+
+    public function note($message);
+
+    public function info($message);
+
+    public function caution($message);
 }
