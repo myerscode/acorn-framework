db680d689b25b83806cd3654c07b0a1cff481d84
Fred Myerscough <oniice@gmail.com>
2022-04-25 10:39:36 +0100

    test(update): app under test path defined

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -6,8 +6,10 @@ use Myerscode\Acorn\Testing\AcornTestCase;
 
 class BaseTestCase extends AcornTestCase
 {
+    protected string $appDirectory = 'tests/Resources/App';
+
     public function runningFrom(): string
     {
-        return dirname(__DIR__) . DIRECTORY_SEPARATOR;
+        return dirname(__DIR__);
     }
 }
