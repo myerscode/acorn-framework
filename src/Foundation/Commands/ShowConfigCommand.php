665b3fe646f48f44b721fb7e70a2c3aedca27e5c
Fred Myerscough <oniice@gmail.com>
2022-10-24 08:29:03 +0100

    refactor(foundation) show config command shows if cached

--- src/Foundation/Commands/ShowConfigCommand.php
+++ src/Foundation/Commands/ShowConfigCommand.php
@@ -3,34 +3,33 @@
 namespace Myerscode\Acorn\Foundation\Commands;
 
 use Myerscode\Acorn\Framework\Console\Command;
-
-use Myerscode\Utilities\Bags\DotUtility;
-
 use Myerscode\Utilities\Bags\Utility;
 
 use function Myerscode\Acorn\Foundation\config;
 
 class ShowConfigCommand extends Command
 {
-
     protected string $signature = 'config';
 
     protected string $description = 'Show all loaded configuration';
 
     public function handle(): void
     {
-        $locations = config('configLocations');
+        if (config('cachedConfig')) {
+            $locations = [config('cachedConfigLocation')];
+        } else {
+            $locations = config('configLocations');
+        }
+
         $bag = new Utility(config());
         $config = $bag->flatten()->toArray();
 
-        $this->output->info("Loaded Config Files From Locations:");
-        foreach ($locations as $location) {
-            $this->output->text($location);
-        }
-//        dd($flattened);
+        $mapped = array_map(fn($key, $value): array => [$value], array_keys($locations), $locations);
+
+        $this->output->table(["Config Loaded From Locations"], $mapped)->render();
+
         $mapped = array_map(fn($key, $value): array => ['key' => $key, 'value' => $value], array_keys($config), $config);
         $headers = ['Key', 'Value'];
-        $this->output->newLine(2);
-        $this->output->table($headers, $mapped);
+        $this->output->table($headers, $mapped)->render();
     }
 }
