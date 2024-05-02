5e3abf3ced83b8db9ea8156c56519c8f10b40253
Fred Myerscough <oniice@gmail.com>
2024-05-02 16:47:59 +0100

    feat(container) added ability to remove container references
    Signed-off-by: Fred Myerscough <oniice@gmail.com>

--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -3,6 +3,8 @@
 namespace Myerscode\Acorn\Framework\Container;
 
 use League\Container\ReflectionContainer;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class Container
 {
@@ -10,10 +12,8 @@ class Container
      * The current globally available container (if any).
      */
     protected static ?Container $instance = null;
-
-    private readonly DependencyManager $container;
-
     protected array $loadedProviders = [];
+    private readonly DependencyManager $container;
 
     public function __construct()
     {
@@ -55,30 +55,6 @@ class Container
     }
 
     /**
-     * Retrieve an instance from the container
-     *
-     * @return mixed
-     *
-     * @see DependencyManager::get()
-     */
-    public function get(string $id): mixed
-    {
-        return $this->container->get($id);
-    }
-
-    /**
-     * Swap the defined container with a new implementation
-     *
-     * @param  string  $id
-     * @param  null  $concrete
-     * @param  bool|null  $shared
-     */
-    public function swap(string $id, $concrete = null, bool $shared = null): void
-    {
-        $this->container->swap($id, $concrete, $shared);
-    }
-
-    /**
      * Load a service provider into the container
      *
      * @param $serviceProvider
@@ -94,12 +70,57 @@ class Container
     }
 
     /**
-     * Returns a list of service providers that have been added to the cotainer
+     * Retrieve an instance from the container
+     *
+     * @param  string  $id
+     *
+     * @return mixed
+     *
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     *
+     * @see DependencyManager::get()
+     */
+    public function get(string $id): mixed
+    {
+        return $this->container->get($id);
+    }
+
+    /**
+     * Returns a list of service providers that have been added to the container
      *
      * @return array
      */
     public function loadedProviders(): array
     {
         return $this->loadedProviders;
+    }
+
+    /**
+     * Remove a service from the container
+     *
+     * @param  string  $id
+     *
+     * @return void
+     *
+     * @see DependencyManager::swap()
+     */
+    public function remove(string $id): void
+    {
+        $this->container->remove($id);
+    }
+
+    /**
+     * Swap the defined instance in the container with a new implementation
+     *
+     * @param  string  $id
+     * @param  null  $concrete
+     * @param  bool|null  $shared
+     *
+     * @see DependencyManager::swap()
+     */
+    public function swap(string $id, $concrete = null, bool $shared = null): void
+    {
+        $this->container->swap($id, $concrete, $shared);
     }
 }
